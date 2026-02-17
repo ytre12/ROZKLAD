21d70095f54210279912b91b0bc2a041fb690ce4
ytre12 <obio12yt@gmail.com>
2026-02-17 21:18:20 +0200

    Add elements of name, number of lessons

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const btnMonday = document.getElementById("btn-mon");
 const btnThusday = document.getElementById("btn-thu");
 
+const lesonName1 = document.querySelector("#leson-1");
+const lesonName2 = document.querySelector("#leson-2");
+const lesonName3 = document.querySelector("#leson-3");
+const lesonName4 = document.querySelector("#leson-4");
+const lesonName5 = document.querySelector("#leson-5");
+
+const audNum1 = document.querySelector("#leson-aud-1");
+const audNum2 = document.querySelector("#leson-aud-2");
+const audNum3 = document.querySelector("#leson-aud-3");
+const audNum4 = document.querySelector("#leson-aud-4");
+const audNum5 = document.querySelector("#leson-aud-5");
+
 const arr = [
   [
     ["Вільно", ""],
