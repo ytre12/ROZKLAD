33bd8b13454e74dcd858c439bb0530ba0bd06e68
ytre12 <obio12yt@gmail.com>
2026-02-17 21:39:16 +0200

    Change bug in eventListener (add normal funk)

--- script.js
+++ script.js
@@ -13,16 +13,40 @@ const audNum3 = document.querySelector("#leson-aud-3");
 const audNum4 = document.querySelector("#leson-aud-4");
 const audNum5 = document.querySelector("#leson-aud-5");
 
-const arr = [
+const domElemArr = [
+  [lesonName1, audNum1],
+  [lesonName2, audNum2],
+  [lesonName3, audNum3],
+  [lesonName4, audNum4],
+  [lesonName5, audNum5],
+];
+
+const textArr = [
   [
-    ["Вільно", ""],
+    ["Вільно", "..."],
     ["Виробничі процеси та обладнання об'єктів автоматизації", "124 аудиторія"],
     ["Основи інтелектуальної власносі", "243 аудиторія"],
     ["Теоритична та прикладна механіка", "279 аудиторія"],
-    ["Вільно", ""],
+    ["Вільно", "..."],
+  ],
+  [
+    ["Вільно", "..."],
+    ["Вільно", "..."],
+    ["Вільно", "..."],
+    ["Вільно", "..."],
+    ["Вільно", "..."],
   ],
   [],
   [],
   [],
-  [],
 ];
+
+const addNewContent = (dayNum) => {
+  for (let i = 0; i < domElemArr.length; i++) {
+    domElemArr[i][0].textContent = textArr[dayNum][i][0];
+    domElemArr[i][1].textContent = textArr[dayNum][i][1];
+  }
+};
+
+btnMonday.addEventListener("click", () => addNewContent(0));
+btnThusday.addEventListener("click", () => addNewContent(1));
